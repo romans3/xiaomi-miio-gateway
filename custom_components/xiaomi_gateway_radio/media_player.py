735a9815bf6c376d3939bd539c21b21909c7983f
Giuseppe Romano <36969283+romans3@users.noreply.github.com>
2026-02-16 19:36:16 +0100

    Fix Error Below
    Detected that custom integration 'xiaomi_gateway_radio' calls `device_registry.async_get_or_create` referencing a non existing `via_device` ('xiaomi_gateway_radio', 'gateway_radio'), with device info: {'connections': {('mac', '78:11:DC:DE:DB:66')}, 'hw_version': 'MW300', 'identifiers': {('xiaomi_gateway_radio', '78:11:DC:DE:DB:66')}, 'manufacturer': 'Xiaomi', 'model': 'lumi.gateway.v3', 'name': 'Xiaomi Gateway Radio', 'sw_version': '1.4.1_176', 'via_device': ('xiaomi_gateway_radio', 'gateway_radio')} at custom_components/xiaomi_gateway_radio/media_player.py, line 56: async_add_entities([entity]). This will stop working in Home Assistant 2025.12.0, please report it to the author of the 'xiaomi_gateway_radio' custom integration

--- custom_components/xiaomi_gateway_radio/media_player.py
+++ custom_components/xiaomi_gateway_radio/media_player.py
@@ -103,7 +103,7 @@ class XiaomiGatewayRadioMediaPlayer(MediaPlayerEntity):
             "model": self._model,
             "sw_version": self._firmware,
             "hw_version": self._hardware,
-            "via_device": (DOMAIN, "gateway_radio"),
+            "via_device": ("xiaomi_miio", self._mac),
         }
 
     # ---------------------------
